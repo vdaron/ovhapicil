d2e5e14dd5b86fe76c5caa7ac010a999c3b96da9
sandrock <antoine.sottiau@gmail.com>
2015-01-25 11:16:51 +0100

    added .NET 4.0 project, fixed assembly infos

---
+++ NET40.OVHApi/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OVHApi.net 4.0")]
+[assembly: AssemblyDescription("Sync .NET 4.0 wrapper for OVH API")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OVHApi.net")]
+[assembly: AssemblyCopyright("The MIT License")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("6c4dd5d2-30ae-4ebe-b8d8-584eb4551618")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
