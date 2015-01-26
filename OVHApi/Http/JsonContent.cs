aa5b80b83a7c813d17f841fb052c40157b8eacc0
sandrock <antoine.sottiau@gmail.com>
2015-01-26 08:39:37 +0100

    fixed JsonContent

--- OVHApi/Http/JsonContent.cs
+++ OVHApi/Http/JsonContent.cs
@@ -4,13 +4,22 @@ namespace OVHApi.Http
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Net;
     using System.Text;
     using System.Threading.Tasks;
 
 #if ASYNC
     using System.Net.Http;
-#endif
+
+    public class JsonContent : StringContent
+    {
+        public JsonContent(string str)
+            : base(str)
+        {
+            base.Headers.ContentType.MediaType = "application/json";
+        }
+    }
+#else
+    using System.Net;
 
     public class JsonContent : StringContent
     {
@@ -22,9 +31,7 @@ namespace OVHApi.Http
         public JsonContent(string content, Encoding encoding)
             : base(content, encoding, "application/json; charset=" + encoding.WebName)
         {
-#if ASYNC
-            base.Headers.ContentType.MediaType = "application/json";
-#endif
         }
     }
+#endif
 }
